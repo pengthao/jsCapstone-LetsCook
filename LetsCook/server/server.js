52c4990faaa52e10a0301815cae23843b1c9778e
unknown <pengsmemberships@gmail.com>
2023-12-27 23:26:32 -0600

    aws

--- LetsCook/server/server.js
+++ LetsCook/server/server.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { SERVER_PORT, rollBarAccessToken } = process.env;
+const path = require('path');
 let options = require("./controller");
-const baseURL = "http://127.0.0.1:8080";
 
 app.use(express.json());
 app.use(cors());
@@ -25,8 +25,9 @@ var rollbar = new Rollbar({
 rollbar.log("Hello world!");
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '../public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 app.get(`/letscook/api/search/`, getRecipes);
 app.get(`/letscook/api/recipe/`, getRecipeDetails);
 app.get(`/letscook/api/list/`, getList);
